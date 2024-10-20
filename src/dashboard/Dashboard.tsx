0368d0751972e97534fc484e746746a426357eb1
ryan thornburg <ryanthornburg@gmail.com>
2024-10-20 02:17:40 -0400

    user colors

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,22 +1,22 @@
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Stack from '@mui/material/Stack';
-import { alpha } from '@mui/material/styles';
-import type { } from '@mui/x-charts/themeAugmentation';
-import type { } from '@mui/x-data-grid/themeAugmentation';
-import type { } from '@mui/x-date-pickers/themeAugmentation';
-import type { } from '@mui/x-tree-view/themeAugmentation';
-import { onValue, ref } from 'firebase/database';
-import { useEffect, useState } from 'react';
-import { db } from '../components/firebase';
-import MainGrid from './components/MainGrid';
-import AppTheme from './shared-theme/AppTheme';
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Stack from "@mui/material/Stack";
+import { alpha } from "@mui/material/styles";
+import type {} from "@mui/x-charts/themeAugmentation";
+import type {} from "@mui/x-data-grid/themeAugmentation";
+import type {} from "@mui/x-date-pickers/themeAugmentation";
+import type {} from "@mui/x-tree-view/themeAugmentation";
+import { onValue, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+import { db } from "../components/firebase";
+import MainGrid from "./components/MainGrid";
+import AppTheme from "./shared-theme/AppTheme";
 import {
   chartsCustomizations,
   dataGridCustomizations,
   datePickersCustomizations,
   treeViewCustomizations,
-} from './theme/customizations';
+} from "./theme/customizations";
 
 const xThemeComponents = {
   ...chartsCustomizations,
@@ -26,34 +26,34 @@ const xThemeComponents = {
 };
 
 export default function Dashboard(props: { disableCustomTheme?: boolean }) {
-  const [weekNum, setWeekNum] = useState(1)
+  const [weekNum, setWeekNum] = useState(1);
 
-  useEffect(()=>{
-    const userRef = ref(db, 'currentWeek')
+  useEffect(() => {
+    const userRef = ref(db, "currentWeek");
     return onValue(userRef, (snapshot) => {
-        if (snapshot.exists()) {
-            setWeekNum(snapshot.val())
-        }
-    })
-  }, [])
+      if (snapshot.exists()) {
+        setWeekNum(snapshot.val());
+      }
+    });
+  }, []);
 
   return (
     <AppTheme {...props} themeComponents={xThemeComponents}>
       <CssBaseline enableColorScheme />
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         {/* Main content */}
         <Box
           component="main"
           sx={(theme) => ({
             flexGrow: 1,
             backgroundColor: alpha(theme.palette.background.default, 1),
-            overflow: 'auto',
+            overflow: "auto",
           })}
         >
           <Stack
             spacing={2}
             sx={{
-              alignItems: 'center',
+              alignItems: "center",
               mx: 3,
               pb: 5,
               mt: { xs: 8, md: 0 },
